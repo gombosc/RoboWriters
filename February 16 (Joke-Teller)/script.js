6d9ca35b822a63a77d8ea42ea917e066ce5d6cac
Cosmin <cosmingombos@gmail.com>
2023-02-17 23:59:07 +0200

    Added new functions
    Now button is disabled while joke is told
    A button now appears which when pressed shows joke text

--- February 16 (Joke-Teller)/script.js
+++ February 16 (Joke-Teller)/script.js
@@ -1,7 +1,14 @@
 const audioElement = document.getElementById('audio');
 const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const container = document.getElementById('container')
+const p = document.getElementById('paragraph')
 // VoiceRSS Javascript SDK
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+
+function toggleButton(){
+    button.disabled = !button.disabled;
+}
 
 function tellJoke(joke){
     VoiceRSS.speech({
@@ -11,22 +18,23 @@ function tellJoke(joke){
         v: 'Linda',
         r: 0, 
         c: 'mp3',
-        f: '44khz_16bit_stereo',
+        f: '44khz_16bit_ stereo',
         ssml: false
     });
 }
 
-function createTextButton(){
-   const button2 = document.createElement('button');
-   button2.textContent = "Show Joke"
-    audioElement.appendChild(button2)
+function showTextButton(){
+   setTimeout(()=>{
+    button2.hidden = false;
+   },1500)
+   ;
 }
 
 // Get Jokes from Jokes API
 async function getJoke(){
-    createTextButton()
     const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
     let joke = '';
+    p.hidden = true;
     try{ 
         const response = await fetch(apiUrl);
         const data = await response.json();
@@ -37,11 +45,28 @@ async function getJoke(){
         }
         console.log(joke)
 
+        // Text-to-Speech
         tellJoke(joke);
+
+        // Button disable
+        toggleButton();
+
+        // Create Joke Text
+        showTextButton()
+
+        // Display Joke Text
+        button2.addEventListener('click', ()=>{
+        button2.hidden = true;
+        p.hidden = !p.hidden;
+        p.innerHTML = joke;})
+        
 }catch(error){
     // console.log("not working: error");
     console.error("Woaaa! Something is not working\n The Error is --- ",error);
 }
 }
 
+
+// Event Listeners
 button.addEventListener("click", getJoke);
+audioElement.addEventListener("ended", toggleButton)
