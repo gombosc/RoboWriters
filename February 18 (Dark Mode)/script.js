a640301d1c38f9f3eaabe93b3699960a4104d4e9
Cosmin <cosmingombos@gmail.com>
2023-02-21 23:18:42 +0200

    Optimized functions with ternary
    Replaced dark and light mode functions with a single one through a ternary operator

--- February 18 (Dark Mode)/script.js
+++ February 18 (Dark Mode)/script.js
@@ -13,32 +13,25 @@ function imageMode(color){
     image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode Styles
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 100%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 75%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
+function toggleDarkLightMode(isDark){
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 100%)' : 'rgb(255 255 255 / 100%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 75%)' : 'rgb(0 0 0 / 75%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark? imageMode('dark') : imageMode('light');
 }
-// Light Mode Style
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 100%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 75%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
-}
+
 function switchTheme(event){
     console.log(event.target.checked);
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme','dark');
-        darkMode();
+        toggleDarkLightMode(true);
     }else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme','light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
