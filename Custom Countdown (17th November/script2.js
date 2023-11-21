acccfe0733ec21053f8182bd45c96af1a62f1fea
Cosmin <cosmingombos@gmail.com>
2023-11-21 23:09:59 +0100

    spend a lot of time trying to figure out how to save the div elements in local storage, as well as retrieving them and creating them once again

--- Custom Countdown (17th November/script2.js
+++ Custom Countdown (17th November/script2.js
@@ -1,28 +1,76 @@
+let mainContainer = document.getElementById("container");
+let secondContainer = document.getElementById("second-container");
 let countdownContainer = document.getElementById("countdown-container")
 let timeElements = document.querySelectorAll("span");
 let titleElement = document.getElementById("countdown-title");
+let countdownButton = document.getElementById("countdown-button");
 
 let countdownTitle;
+let divNr = 0;
 
-function restrieveLocalItems(){
+function restrieveLocalItems(div){
     if(localStorage.getItem("countdownStorage")){
+        countdownButton.textContent = "Show";
         countdownStorage = JSON.parse(localStorage.getItem('countdownStorage'));
         titleElement.hidden = false;
         countdownTitle = countdownStorage.title;
         titleElement.textContent = countdownTitle;
-        // countdownDate = countdownStorage.date;
-        // userHour = countdownStorage.hour;
-        // dateUserValue = new Date(`${countdownDate}T${userHour}`).getTime();
-        // inputContainer.hidden = true;
-        // countdownTimer.hidden = false;
     }else{
         countdownContainer.hidden = false;
     }
 }
 
-function updateDOM(){
-    
+function addSavedDivs(){
+    if(localStorage.getItem("countdown0")){
+        for(i=0; i<=10; i++){
+            // Check for each saved countdown div and add it to second container
+            let countdown = `countdown${i}`;
+            if(localStorage.getItem(countdown)){
+                let countdownEl = localStorage.getItem(countdown);
+                secondContainer.innerHTML = secondContainer.innerHTML + countdownEl;
+            }
+        }
+    }    
+}
+
+
+function onPlusSign(){
+        let newCountdownDiv = document.createElement("div");
+        newCountdownDiv.id = 'countdown-container';
+        secondContainer.appendChild(newCountdownDiv);
+
+        // Add the title to container 
+        let newCountdownTitle = document.createElement("h1");
+        newCountdownTitle.id = 'countdown-title';
+        newCountdownTitle.textContent = "Test";
+        newCountdownDiv.appendChild(newCountdownTitle);
+        
+        // Add Button to id
+        let newButton = document.createElement('button');
+        newButton.id = "countdown-button"
+        newButton.textContent = "Select";
+        newCountdownDiv.appendChild(newButton);
+        
+        // save div to local storage 
+        saveDivToLocalStorage(newCountdownDiv)
+
+        divNr++;
+}
+
+
+
+function saveDivToLocalStorage(div){
+    let countdownDiv = `countdown${divNr}`;
+    // Save to local storage
+    localStorage.setItem(countdownDiv, div.outerHTML);
+}
+
+function removeSavedDivs(){
+    localStorage.clear();
 }
 
 // onLoad retrieve user countdown
-restrieveLocalItems();
+window.onload = (event) => {
+    restrieveLocalItems();
+    addSavedDivs();
+  };
